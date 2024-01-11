4e8fe955a26806b11b488d93040866c269c63a42
gmbarsoti <gmbarsoti@gmail.com>
2024-01-10 21:51:53 -0300

    Inserting minor changes
    Minor changes

--- nu_funcs.py
+++ nu_funcs.py
@@ -2,19 +2,29 @@ import os
 import csv
 
 
-class Pix:
-    def __init__(self, date, description, card_info, value, tags=None):
+class FinancialTransaction:
+    def __int__(self, date, recipient, depositor, description, value, type, tags=None):
         if tags is None:
             tags = []
         self.date = date
+        self.recipient = recipient
+        self.depositor = depositor
         self.description = description
-        self.card_info = card_info
         self.value = value
+        self.type = type
         self.tags = tags
+
+
+class Pix(FinancialTransaction):
+    def __init__(self, message, received, institution):
+        self.message = message
+        self.received = received
+        self.institution = institution
+
 
 def csv_to_obj():
     nu_csv_path = os.path.join('.', 'source', 'Nu', 'nu.csv')
-
+    pix_list = []
     with open(nu_csv_path, 'r', encoding='UTF-8') as nu_csv:
         nu_csv_dict = csv.DictReader(nu_csv)
         line_count = 0
@@ -25,6 +35,9 @@ def csv_to_obj():
             print(
                 f'\t{row["date"]} - {row["value"]} - {row["description"]}.')
             line_count += 1
+            #getting pixs
+            if "Pix" in row["description"]:
+                pix_list.append(Pix(row["description"], ))
         print(f'Processed {line_count} lines.')
 
 

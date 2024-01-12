5f52e8290e6212e42691c6b408b93ed4288e339a
gmbarsoti <gmbarsoti@gmail.com>
2024-01-12 00:01:17 -0300

    Improving function to parse and build pix obj

--- nu_funcs.py
+++ nu_funcs.py
@@ -1,25 +1,60 @@
 import os
 import csv
+from enum import Enum
+
+
+class TransactionType(Enum):
+    PIX = 1
+    PAYMENT = 2
 
 
 class FinancialTransaction:
-    def __int__(self, date, recipient, depositor, description, value, type, tags=None):
+    def __init__(self, date, recipient, depositor, value, type, tags=None):
         if tags is None:
             tags = []
         self.date = date
         self.recipient = recipient
         self.depositor = depositor
-        self.description = description
         self.value = value
         self.type = type
         self.tags = tags
 
 
 class Pix(FinancialTransaction):
-    def __init__(self, message, received, institution):
-        self.message = message
+    def __init__(self, date, recipient, depositor, value, type, received, institution, message="", tags=None):
+        if tags is None:
+            tags = []
+        super().__init__(date, recipient, depositor, value, type, tags)
         self.received = received
         self.institution = institution
+        self.message = message
+
+
+
+def parse_pix_description(description, received):
+    elements = description.split(" - ")
+
+    recipient = ""
+    depositor = ""
+    if received:
+        recipient = "Account owner"
+        depositor = elements[1]
+    else:
+        recipient = elements[1]
+        depositor = "Account owner"
+
+    if len(elements) >4:
+        elements[3] += elements[4]
+
+    pix_description = {
+        "General": elements[0],
+        "Recipient": recipient,
+        "Depositor": depositor,
+        "key piece": elements[2],
+        "Institution": elements[3],
+        "Received": received
+    }
+    return pix_description
 
 
 def csv_to_obj():
@@ -36,10 +71,21 @@ def csv_to_obj():
                 f'\t{row["date"]} - {row["value"]} - {row["description"]}.')
             line_count += 1
             #getting pixs
-            if "Pix" in row["description"]:
-                pix_list.append(Pix(row["description"], ))
+            pix_pattern = " Pix -"
+            if pix_pattern in row["description"]:
+                #parse pix description
+                received = True
+                if float(row["value"]) < 0.0:
+                    received = False
+                pix_description = parse_pix_description(row["description"], received)
+                pix_list.append(Pix(row["date"], pix_description["Recipient"],
+                                    pix_description["Depositor"], float(row["value"]),
+                                    TransactionType.PIX, pix_description["Received"],
+                                    pix_description["Institution"], ""))
         print(f'Processed {line_count} lines.')
 
 
 if __name__ == '__main__':
     csv_to_obj()
+    #pix_obj = Pix("eu", "Nu", "selva")
+    pass
